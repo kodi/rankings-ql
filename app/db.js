6d95ddad1cddda54e01b7b20b21ae8b6fda7dc6e
kodi <dragan@bajcic.com>
2015-12-08 21:41:32 +0100

    split files

---
+++ app/db.js
@@ -0,0 +1,12 @@
+var mysql      = require('mysql');
+
+var pool = mysql.createPool({
+    connectionLimit : 10,
+    host     : '127.0.0.1',
+    user     : 'root',
+    password : '',
+    database : 'ratings_test'
+});
+
+
+module.exports = pool;
