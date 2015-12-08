d9e4f2e4eeb054c5055e5fc318611b688d028ee3
kodi <dragan@bajcic.com>
2015-12-08 19:15:59 +0100

    logger

--- server.js
+++ server.js
@@ -2,10 +2,13 @@ var express    = require('express');        // call express
 var mysql      = require('mysql');
 var _          = require('lodash');
 var chalk = require('chalk');
+var expressWinston = require('express-winston');
+var winston = require('winston'); // for transports.Console
 
 var app        = express();                 // define our app using express
 var port       = 4444;
-var connection = mysql.createConnection({
+var pool = mysql.createPool({
+    connectionLimit : 10,
     host     : '127.0.0.1',
     user     : 'root',
     password : '',
@@ -24,69 +27,100 @@ router.get('/omega/elo/:ids', function(req, res) {
 
     var IN = ids.join(',');
 
-    //console.log(IN);
-    connection.query("SELECT * FROM player_rank where player_id IN (" + IN + ")", function(err, result){
+    console.log(req.ip);
+    pool.getConnection(function(err, connection) {
+        // Use the connection
+        connection.query("SELECT * FROM player_rank where player_id IN (" + IN + ")", function(err, result) {
+            // And done with the connection.
 
-        //console.log(result);
+            //console.log(result);
 
-        var playerIndex = {};
-        _.each(result, function(player){
-
-
-            var p = {
-                steamid : player.player_id,
-                ctf :{
-                    elo : player.rank,
-                    games : player.num_games
-
-                }
-            };
-
-            playerIndex[player.player_id] = true;
-            players.push(p);
-
-        });
+            var playerIndex = {};
+            _.each(result, function(player){
 
 
-        //apply the cap
-        _.each(players, function(player){
-
-            if(player.ctf.elo < 250) {
-                player.ctf.elo = 250;
-            }
-
-            if(player.ctf.elo > 750) {
-                player.ctf.elo = 750;
-            }
-
-        });
-
-
-        //new players
-        _.each(ids, function(id){
-            if(typeof playerIndex[id] === 'undefined'){
                 var p = {
-                    steamid : id,
+                    steamid : player.player_id,
                     ctf :{
-                        elo : 275,
-                        games : 0
+                        elo : player.rank,
+                        games : player.num_games
+
                     }
                 };
 
+                playerIndex[player.player_id] = true;
                 players.push(p);
-            }
+
+            });
+
+
+            //apply the cap
+            _.each(players, function(player){
+
+                if(player.ctf.elo < 250) {
+                    player.ctf.elo = 250;
+                }
+
+                if(player.ctf.elo > 750) {
+                    player.ctf.elo = 750;
+                }
+
+            });
+
+
+            //new players
+            _.each(ids, function(id){
+                if(typeof playerIndex[id] === 'undefined'){
+                    var p = {
+                        steamid : id,
+                        ctf :{
+                            elo : 275,
+                            games : 0
+                        }
+                    };
+
+                    players.push(p);
+                }
+
+            });
+
+            res.send(JSON.stringify({players: players}));
+
+            console.log('done, releaseing connection');
+            connection.release();
+
 
         });
-
-        res.send(JSON.stringify({players: players}));
-
     });
+
 
 });
 
 
+
+// express-winston logger makes sense BEFORE the router.
+app.use(expressWinston.logger({
+    transports: [
+        new winston.transports.Console({
+            json: true,
+            colorize: true
+        })
+    ]
+}));
+
+
 app.use('/api', router);
 
+
+// express-winston errorLogger makes sense AFTER the router.
+app.use(expressWinston.errorLogger({
+    transports: [
+        new winston.transports.Console({
+            json: true,
+            colorize: true
+        })
+    ]
+}));
 
 // START THE SERVER
 // =============================================================================
