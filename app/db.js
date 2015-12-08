1de85daff7b0c7294dffd64d4de9f0bdbb7cba22
kodi <dragan@bajcic.com>
2015-12-08 21:46:41 +0100

    db

--- app/db.js
+++ app/db.js
@@ -4,7 +4,7 @@ var pool = mysql.createPool({
     connectionLimit : 10,
     host     : '127.0.0.1',
     user     : 'root',
-    password : '',
+    password : 'd0dct0rwh0',
     database : 'ratings_test'
 });
 
