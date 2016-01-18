87d13bb1f3326ae120f9a027e1837d1787940c89
kodi <dragan@bajcic.com>
2016-01-18 14:16:08 +0100

    fix date back

--- cli/get_matches.js
+++ cli/get_matches.js
@@ -19,8 +19,8 @@ var LoopArray = require('../lib/arr_loop');
 
 //step 1, read all files
 
-//var date = moment().format('YYYY-MM-DD');
-var date = '2016-01-17';
+var date = moment().format('YYYY-MM-DD');
+//var date = '2016-01-17';
 LOG.logOk("Getting the games for date: " + date);
 var api = new QlstatsApi();
 api.getAllData(date, function (data) {
